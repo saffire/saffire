9d7af8e17ea92e8ae5e32fb9e67356cdb543e3b8
Joshua Thijssen <jthijssen@noxlogic.nl>
2012-07-28 15:47:11 +0200

    Initial commit with nodes

--- src/main.c
+++ src/main.c
@@ -1,213 +1,116 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "node.h"
 #include "parser.tab.h"
-
 #include "saffire_parser.h"
+
+#include "svar.h"
 
 extern int yyparse();
 extern FILE *yyin;
 
 
-const int VT_NULL   = 0;
-const int VT_LONG   = 1;
-const int VT_STRING = 2;
-
-typedef struct var {
-    char type;
-    char *name;
-    union {
-        long num;
-        char *str;
-
-    } val;
-} t_var;
-
-#define MAX_VARS 50
-t_var *vars[MAX_VARS];
-
-void print_var(t_var *var) {
-    printf("VAR:\n");
-    printf("  Name: %s\n", var->name);
-    printf("  Type: %d\n", var->type);
-    if (var->type == VT_LONG) {
-        printf("  Val:  %ld\n", var->val.num);
-    }
-    if (var->type == VT_STRING) {
-        printf("  Val:  %s\n", var->val.str);
-    }
-    printf("\n");
+void saffire_do_program_begin(char *title) {
+}
+void saffire_do_program_end() {
 }
 
+void saffire_do_assign(char *var_name, char *val) {
+    // Default to a string
+    char type = SV_STRING;
 
+    // Check if the WHOLE val is a numerical value
+    char *endptr;
+    long num = strtol(val, &endptr, 10);
+    if (endptr == val+strlen(val)) {
+        // only when the whole value is converted, it's a long
+        // "20" is a long, "20A" isn't.
+        type = SV_LONG;
+    }
 
-
-
-int var_find_slot() {
-    for (int i = 0; i !=MAX_VARS; i++) {
-        if (vars[i] == NULL) {
-            return i;
+    // Try and search for this value
+    svar *var = svar_find(var_name);
+    if (var == NULL) {
+        //  Not found, initial assignment
+        var = svar_alloc(type, var_name, type == SV_STRING ? (void *)val : (void *)num);
+    } else {
+        // Found, check if types match: cannot change from string to int for example
+        if (var->type != type) {
+            printf("We cannot switch types!");
+            exit(1);
+        }
+        // add number
+        if (type == SV_STRING) {
+            var->val.s = strdup(val);
+        } else if (type == SV_LONG) {
+            var->val.l = num;
         }
     }
-    return -1;
 }
 
-t_var *var_find(char *name) {
-    int i;
-    for (i=0; i!=MAX_VARS; i++) {
-        if (vars[i] == NULL) continue;
 
-        if (strcmp(vars[i]->name, name) == 0) {
-            return vars[i];
+void saffire_do_print(char *str) {
+    if (str[0] == '$') {
+        svar *var = svar_find(str);
+        if (var == NULL) {
+            printf("Cannot find variable %s", str);
+            exit(1);
         }
-    }
-
-    return NULL;
-}
-
-t_var *var_alloc(char type, char *name, void *val) {
-    int var_idx = var_find_slot();
-    if (var_idx == -1) {
-        fprintf(stderr, "No more room for variables!");
-        exit(1);
-    }
-    t_var *var = (t_var *)malloc(sizeof(t_var));
-    var->type = type;
-    var->name = strdup(name);
-
-    if (var->type == VT_LONG) {
-        var->val.num = (long)val;
-    } else if (var->type == VT_STRING) {
-        var->val.str = strdup((char *)val);
-    }
-
-    vars[var_idx] = var;
-    return var;
-}
-
-void var_free(t_var *var) {
-    free(var->name);
-    if (var->type == VT_STRING) {
-        free(var->val.str);
+        if (var->type == SV_STRING) {
+            printf("%s", var->val.s);
+        } else if (var->type == SV_LONG) {
+            printf("%ld", var->val.l);
+        } else if (var->type == SV_DOUBLE) {
+            printf("%g", var->val.d);
+        }
+    } else {
+        //str[strlen(str)-1] = '\0';
+        //printf("%s", str+1);
+        printf("%s", str);
     }
 }
 
 void _do_incdec(char *var_name, int inc) {
-    t_var *var = var_find(var_name);
+    svar *var = svar_find(var_name);
     if (var == NULL) {
         printf("Warning: var is not initialized.");
-        var = var_alloc(VT_LONG, var_name, 0);
+        var = svar_alloc(SV_LONG, var_name, 0);
     }
 
-    if (var->type != VT_LONG) {
+    if (var->type != SV_LONG) {
         printf("Warning: var is not a number");
         return;
     }
 
     if (inc) {
-        var->val.num++;
+        var->val.l++;
     } else {
-        var->val.num--;
+        var->val.l--;
     }
 
     //print_var(var);
-}
-
-void saffire_do_program_begin(char *title) {
-    //printf("do_program_begin: %s \n", title);
-}
-void saffire_do_program_end() {
-    //printf("do_program_end\n");
-}
-
-void saffire_do_assign(char *var_name, char *val) {
-    //printf("assign(%s => %s)\n", var_name, val);
-
-
-    char type = VT_STRING;
-    char *endptr;
-    long num = strtol(val, &endptr, 10);
-    if (endptr == val+strlen(val)) {
-        type = VT_LONG;
-    }
-
-
-    t_var *var = var_find(var_name);
-    if (var == NULL) {
-        printf("Initial assign\n");
-        var = var_alloc(type, var_name, type == VT_STRING ? (void *)val : (void *)num);
-    } else {
-        if (var->type != type) {
-            printf("We cannot switch types!");
-            exit(1);
-        }
-        if (type == VT_STRING) {
-            var->val.str = strdup(val);
-        } else if (type = VT_LONG) {
-            var->val.num = num;
-        }
-    }
-
-    //print_var(var);
-}
-
-
-void saffire_do_print(char *str) {
-    //printf("print(%s)\n", str);
-
-    if (str[0] == '$') {
-        t_var *var = var_find(str);
-        if (var == NULL) {
-            printf("Cannot find variable %s", str);
-            exit(1);
-        }
-        if (var->type == VT_STRING) {
-            printf("%s", var->val.str);
-        } else if (var->type = VT_LONG) {
-            printf("%ld", var->val.num);
-        }
-    } else {
-        str[strlen(str)-1] = '\0';
-        printf("%s", str+1);
-    }
 }
 
 void saffire_do_pre_inc(char *var_name) {
-    //printf("do_pre_inc()\n");
     _do_incdec(var_name, 1);
 }
 
 void saffire_do_post_inc(char *var_name) {
-    //printf("do_post_inc()\n");
     _do_incdec(var_name, 1);
 }
 
 void saffire_do_pre_dec(char *var_name) {
-    //printf("do_pre_dec()\n");
     _do_incdec(var_name, 0);
 }
 
 void saffire_do_post_dec(char *var_name) {
-    //printf("do_post_dec()\n");
     _do_incdec(var_name, 0);
 }
 
 
-void saffire_inner_statement() {
-    //printf("inner_statement();\n");
-}
-
-void saffire_do_expr() {
-    //printf("expr\n");
-}
-
-
 int main(int argc, char *argv[]) {
-    // Here we just initialize some temporary variable storage
-    int i;
-    for (i=0; i!=MAX_VARS; i++) {
-        vars[i] = NULL;
-    }
+    svar_init_table();
 
     // Usage
     if (argc < 2) {
