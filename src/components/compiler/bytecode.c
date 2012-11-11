3f996e734750f2e5baa1f7e4b811d14e3b1bb1ba
Joshua Thijssen <jthijssen@noxlogic.nl>
2012-11-11 23:00:55 +0100

    temp checkin for bytecode to disk

--- src/components/compiler/bytecode.c
+++ src/components/compiler/bytecode.c
@@ -33,268 +33,7 @@
 #include "general/smm.h"
 #include "version.h"
 #include "vm/vm_opcodes.h"
-
-//
-///**
-// * Check if a constant already is present inside the bytecode structure
-// */
-//static int _bytecode_constant_exists(t_bytecode *bc, int type, void *value) {
-//    t_dll_element *e = DLL_HEAD(bc->constants_dll);
-//    long l;
-//
-//    while (e) {
-//        t_bytecode_constant *constant = e->data;
-//        if (constant->hdr.type == type) {
-//            switch (constant->hdr.type) {
-//                case BYTECODE_CONST_NULL :
-//                    return 1;
-//                    break;
-//                case BYTECODE_CONST_STRING :
-//                    if (strcmp(constant->data.s, value) == 0) return 1;
-//                    break;
-//                case BYTECODE_CONST_NUMERICAL :
-//                    printf("Checking %d against %d\n", constant->data.l, (long)value);
-//                    if (constant->data.l == (long)value) return 1;
-//                    break;
-//                case BYTECODE_CONST_BOOLEAN :
-//                    if (constant->data.l == (long)value) return 1;
-//                    break;
-//                case BYTECODE_CONST_REGEX :
-//                    break;
-//            }
-//        }
-//        e = DLL_NEXT(e);
-//    }
-//
-//    return 0;
-//}
-//
-///**
-// * Parse the AST and add all constants
-// */
-//static void _bytecode_parse_constants(t_ast_element *p, t_bytecode *bc) {
-//    t_bytecode_constant *constant = NULL;
-//    if (!p) return;
-//
-//    switch (p->type) {
-////        case typeAstString :
-////            printf("STRING: '%s'\n", p->string.value);
-////            if (! _bytecode_constant_exists(bc, BYTECODE_CONST_STRING, p->string.value)) {
-////                constant = (t_bytecode_constant *)smm_malloc(sizeof(t_bytecode_constant));
-////                constant->hdr.type = BYTECODE_CONST_STRING;
-////                constant->hdr.length = strlen(p->string.value);
-////                constant->data.s = smm_strdup(p->string.value);
-////                dll_append(bc->constant_dll, constant);
-////            }
-////            break;
-////        case typeAstNumerical :
-////            if (! _bytecode_constant_exists(bc, BYTECODE_CONST_NUMERICAL, (void *)p->numerical.value)) {
-////                constant = (t_bytecode_constant *)smm_malloc(sizeof(t_bytecode_constant));
-////                constant->hdr.type = BYTECODE_CONST_NUMERICAL;
-////                constant->hdr.length = sizeof(long);
-////                constant->data.l = p->numerical.value;
-////                dll_append(bc->constant_dll, constant);
-////            }
-////            break;
-////        case typeAstIdentifier :
-////            if (p->string.value[0] == '$') break;
-////            printf("IDENT: '%s'\n", p->string.value);
-////            if (! _bytecode_constant_exists(bc, BYTECODE_CONST_STRING, p->identifier.name)) {
-////                constant = (t_bytecode_constant *)smm_malloc(sizeof(t_bytecode_constant));
-////                constant->hdr.type = BYTECODE_CONST_STRING;
-////                constant->hdr.length = strlen(p->identifier.name);
-////                constant->data.s = smm_strdup(p->identifier.name);
-////                dll_append(bc->constant_dll, constant);
-////            }
-////            break;
-////        case typeNull :
-////            if (! _bytecode_constant_exists(bc, BYTECODE_CONST_NULL, NULL)) {
-////                constant = (t_bytecode_constant *)smm_malloc(sizeof(t_bytecode_constant));
-////                constant->hdr.type = BYTECODE_CONST_NULL;
-////                constant->hdr.length = 0;
-////                constant->data.l = 0;
-////                dll_append(bc->constant_dll, constant);
-////            }
-////            break;
-////        case typeOpr :
-////            // Plot all the operands
-////            for (int i=0; i!=p->opr.nops; i++) {
-////                _bytecode_parse_constants(p->opr.ops[i], bc);
-////            }
-////            break;
-////        case typeInterface :
-////            _bytecode_parse_constants(p->interface.implements, bc);
-////            _bytecode_parse_constants(p->interface.body, bc);
-////            break;
-////        case typeClass :
-////            _bytecode_parse_constants(p->class.extends, bc);
-////            _bytecode_parse_constants(p->class.implements, bc);
-////            _bytecode_parse_constants(p->class.body, bc);
-////            break;
-////        case typeMethod:
-////            _bytecode_parse_constants(p->method.arguments, bc);
-////            _bytecode_parse_constants(p->method.body, bc);
-////            break;
-//        default :
-//            printf("Unknown type!");
-//            exit(1);
-//            break;
-//    }
-//}
-//
-//
-///**
-// * Returns the length of byte from a (filled) bytecode structure.
-// */
-//static long bytecode_calculate_length(t_bytecode *bc) {
-//    t_dll_element *e;
-//    long len = sizeof(t_bytecode_header);
-//
-//    // Calculate and add constant lengths
-//    if (bc->constant_dll) {
-//        e = DLL_HEAD(bc->constant_dll);
-//        for (int i=0; i!=bc->constant_dll->size; i++) {
-//            t_bytecode_constant *constant = e->data;
-//            len += sizeof(t_bytecode_constant_header) + constant->hdr.length;
-//            e = DLL_NEXT(e);
-//        }
-//    }
-//
-//    // Calculate and add class lengths
-//    if (bc->class_dll) {
-//        e = DLL_HEAD(bc->class_dll);
-//        for (int i=0; i!=bc->class_dll->size; i++) {
-//            t_bytecode_constant *constant = e->data;
-//            len += sizeof(t_bytecode_constant_header) + constant->hdr.length;
-//            e = DLL_NEXT(e);
-//        }
-//    }
-//
-//    return len;
-//}
-//
-///**
-// *
-// */
-//static void bytecode_generate_buffer(t_bytecode *bc) {
-//    t_dll_element *e;
-//    int bufpos = 0;
-//
-//    // Create our binary bytecode buffer
-//    bc->length = bytecode_calculate_length(bc);
-//    bc->buffer = (char *)smm_malloc(bc->length);
-//
-//    // Skip the header. We still need to fill some vars first
-//    bufpos = sizeof(t_bytecode_header);
-//
-//    printf("CDS: %d\n", bc->constant_dll->size);
-//    printf("CDO: %08X (%d)\n", bufpos, bufpos);
-//
-//    // Add constant header vars
-//    bc->header->constant_count = bc->constant_dll->size;
-//    bc->header->constant_offset = bufpos;
-//
-//    // Add all constant entries
-//    e = DLL_HEAD(bc->constant_dll);
-//    for (int i=0; i!=bc->constant_dll->size; i++) {
-//        t_bytecode_constant *constant = e->data;
-//
-//        // Save header
-//        memcpy(bc->buffer+bufpos, &constant->hdr, sizeof(t_bytecode_constant_header));
-//        bufpos += sizeof(t_bytecode_constant_header);
-//
-//        // Save additional data
-//        if (constant->hdr.length) {
-//            if (constant->hdr.type == BYTECODE_CONST_STRING || constant->hdr.type == BYTECODE_CONST_REGEX) {
-//                memcpy(bc->buffer+bufpos, constant->data.s, constant->hdr.length);
-//            } else {
-//                memcpy(bc->buffer+bufpos, &constant->data.l, constant->hdr.length);
-//            }
-//            bufpos += constant->hdr.length;
-//        }
-//
-//        e = DLL_NEXT(e);
-//    }
-//
-//
-//    // Add class header vars
-//    bc->header->class_count = 0x0;
-//    bc->header->class_offset = bufpos;
-//
-//    // Add all constant entries
-//    e = DLL_HEAD(bc->class_dll);
-//    // @TODO: fill this
-//
-//    // Finally, go back and fill the header.
-//    bufpos = 0;
-//    memcpy(bc->buffer+bufpos, bc->header, sizeof(t_bytecode_header));
-//}
-//
-//
-//
-///**
-// * Allocate the bytecode structure
-// */
-//static t_bytecode *bytecode_structure_alloc(void) {
-//    t_bytecode *bc = (t_bytecode *)smm_malloc(sizeof(t_bytecode));
-//
-//    bc->length = 0;
-//    bc->header = (t_bytecode_header *)smm_malloc(sizeof(t_bytecode_header));
-//    bc->constant_dll = dll_init();
-//    bc->class_dll = dll_init();
-//
-//    memset(bc->header, 0, sizeof(t_bytecode_header));
-//
-//    return bc;
-//}
-//
-//
-///**
-// * Release the bytecode structure
-// */
-//void bytecode_free(t_bytecode *bc) {
-//    dll_free(bc->class_dll);
-//    dll_free(bc->constant_dll);
-//    smm_free(bc->header);
-//    smm_free(bc);
-//}
-//
-//
-///**
-// * Generate a complete binary bytecode buffer from an AST.
-// */
-//t_bytecode *bytecode_generate(t_ast_element *p, char *source_file) {
-//    t_bytecode *bc = bytecode_structure_alloc();
-//
-//    // Fetch modification time from source file and fill into header
-//    struct stat sb;
-//    if (! stat(source_file, &sb)) {
-//        bc->header->timestamp = sb.st_mtime;
-//    }
-//    bc->header->magic = MAGIC_HEADER;
-//    bc->header->version = saffire_version_binary;
-//
-//
-//    _bytecode_parse_constants(p, bc);
-//    //_bytecode_parse_classes(p, bc);
-//
-//    bytecode_generate_buffer(bc);
-//    return bc;
-//}
-//
-//
-//char *bytecode_generate_destfile(const char *src) {
-//    printf("SRC: '%s'\n", src);
-//    char *dst = (char *)smm_malloc(strlen(src)+2);
-//    memset(dst, 0, strlen(src)+2);
-//    strcpy(dst,src);
-//    dst[strlen(dst)] = 'c'; // Add extra c to create *.sfc
-//
-//    printf("DST: '%s'\n", dst);
-//    return dst;
-//}
-
-
+#include "interpreter/errors.h"
 
 
 /**
@@ -311,6 +50,7 @@ static void _new_constant(t_bytecode *bc, int type, int len, void *data) {
     bc->constants_len++;
 }
 
+
 /**
  * Add a new variable to the bytecode structure
  */
@@ -323,6 +63,168 @@ static void _new_variable(t_bytecode *bc, char *var) {
     bc->variables[bc->variables_len] = c;
     bc->variables_len++;
 }
+
+
+static void _read_buffer(char *buf, int *bufptr, int size, void *data) {
+    memcpy(data, buf+*bufptr, size);
+    *bufptr += size;
+}
+
+static void _write_buffer(char *buf, int *bufptr, int size, void *data) {
+    memcpy(buf+*bufptr, data, size);
+    *bufptr += size;
+}
+
+
+/**
+ * Convert binary stream to a bytecode structure (NOTE: bytecode is an unallocated pointer!)
+ */
+int convert_binary_to_bytecode(int bincode_len, char *bincode, t_bytecode *bytecode) {
+    int pos = 0;
+
+    bytecode = (t_bytecode *)smm_malloc(sizeof(t_bytecode));
+    bzero(bytecode, sizeof(bytecode));
+
+    _read_buffer(bincode, &pos, sizeof(int), (void *)bytecode->stack_size);
+    _read_buffer(bincode, &pos, sizeof(int), (void *)bytecode->code_len);
+    _read_buffer(bincode, &pos, bytecode->code_len, (void *)bytecode->code);
+
+    _read_buffer(bincode, &pos, sizeof(int), (void *)bytecode->constants_len);
+    for (int i=0; i!=bytecode->constants_len; i++) {
+        char type; int len; char *ptr;
+        _read_buffer(bincode, &pos, sizeof(char), &type);
+        _read_buffer(bincode, &pos, sizeof(int), &len);
+        _read_buffer(bincode, &pos, len, ptr);
+        _new_constant(bytecode, type, len, ptr);
+    }
+
+    _read_buffer(bincode, &pos, sizeof(int), (void *)bytecode->variables_len);
+    for (int i=0; i!=bytecode->variables_len; i++) {
+        int len; char *ptr;
+        _read_buffer(bincode, &pos, sizeof(int), (void *)&len);
+        _read_buffer(bincode, &pos, len, (void *)ptr);
+        _new_variable(bytecode, ptr);
+    }
+
+    return 1;
+}
+
+
+/**
+ * Convert bytecode structure into a binary stream (NOTE: bincode is an unallocated pointer!)
+ */
+int convert_bytecode_to_binary(t_bytecode *bytecode, int *bincode_len, char *bincode) {
+    _write_buffer(bincode, bincode_len, sizeof(int), (void *)bytecode->stack_size);
+    _write_buffer(bincode, bincode_len, sizeof(int), (void *)bytecode->code_len);
+    _write_buffer(bincode, bincode_len, bytecode->code_len, (void *)bytecode->code);
+    _write_buffer(bincode, bincode_len, sizeof(int), (void *)bytecode->constants_len);
+    for (int i=0; i!=bytecode->constants_len; i++) {
+        _write_buffer(bincode, bincode_len, sizeof(char), (void *)bytecode->constants[i]->type);
+        _write_buffer(bincode, bincode_len, sizeof(int), (void *)bytecode->constants[i]->len);
+        _write_buffer(bincode, bincode_len, bytecode->constants[i]->len, (void *)bytecode->constants[i]->data.ptr);
+    }
+
+    _write_buffer(bincode, bincode_len, sizeof(int), (void *)bytecode->variables_len);
+    for (int i=0; i!=bytecode->variables_len; i++) {
+        _write_buffer(bincode, bincode_len, sizeof(int), (void *)bytecode->variables[i]->len);
+        _write_buffer(bincode, bincode_len, bytecode->variables[i]->len, (void *)bytecode->variables[i]->s);
+    }
+
+    return 1;
+}
+
+
+/**
+ * Load a bytecode from disk, optionally verify signature
+ */
+t_bytecode *load_bytecode_from_disk(const char *filename, int verify_signature) {
+    FILE *f = fopen(filename, "rb");
+
+    t_bytecode_binary_header header;
+    fread(&header, sizeof(header), 1, f);
+
+    char *bincode = (char *)smm_malloc(header.bytecode_len);
+    fseek(f, header.bytecode_offset, SEEK_SET);
+    fread(bincode, header.bytecode_len, 1, f);
+
+    if (verify_signature == 0 && header.signature_offset != 0) {
+        saffire_warning("A signature is present, but verification is disabled");
+    }
+
+    if (header.signature_offset != 0 && verify_signature) {
+        // Read signature
+        char *signature = (char *)smm_malloc(header.signature_len);
+        fseek(f, header.signature_offset, SEEK_SET);
+        fread(signature, header.signature_len, 1, f);
+
+        // @TODO: Verify!
+        printf("@TODO: verify the signature!");
+    }
+
+    fclose(f);
+
+    // Convert binary to bytecode
+    t_bytecode *bc = NULL;
+    if (! convert_binary_to_bytecode(header.bytecode_len, bincode, bc)) {
+        saffire_error("Could not convert bytecode data");
+    }
+
+    // Return bytecode
+    return bc;
+}
+
+
+/**
+ * Save a bytecode from disk, optionally sign and add signature
+ */
+void save_bytecode_to_disk(const char *dest_filename, const char *source_filename, t_bytecode *bc, int sign) {
+    // Convert
+    int bincode_len;
+    char *bincode = NULL;
+    if (! convert_bytecode_to_binary(bc, &bincode_len, bincode)) {
+        saffire_error("Could not convert bytecode data");
+    }
+
+    t_bytecode_binary_header header;
+    header.magic = MAGIC_HEADER;
+
+    // Fetch modification time from source file and fill into header
+    struct stat sb;
+    if (! stat(source_filename, &sb)) {
+        header.timestamp = sb.st_mtime;
+    } else {
+        header.timestamp = 0;
+    }
+
+    // Create file
+    FILE *f = fopen(dest_filename, "wb");
+
+    // temporary write header
+    fwrite("\0", 1, sizeof(header), f);
+
+    // Write bytecode
+    header.bytecode_len = bincode_len;
+    header.bytecode_offset = ftell(f);
+    fwrite(bincode, bincode_len, 1, f);
+
+    // Add signature at the end of the file
+    if (sign == 1) {
+        // Create signature
+        int gpg_signature_len = 0;
+        char *gpg_signature = "blaat";
+
+        header.signature_offset = ftell(f);
+        header.signature_len = gpg_signature_len;
+        fwrite(gpg_signature, gpg_signature_len, 1, f);
+    }
+
+    // Reset to the start of the file and write header
+    fseek(f, 0, SEEK_SET);
+    fwrite(&header, sizeof(header), 1, f);
+
+    fclose(f);
+}
+
 
 
 /**
@@ -378,6 +280,9 @@ t_bytecode *generate_dummy_bytecode(void) {
     _new_constant(bc, BYTECODE_CONST_OBJECT, 4, "print");
     _new_variable(bc, "a");
     _new_variable(bc, "b");
+
+
+    save_bytecode_to_disk("bytecode.sfc", "bytecode.sf", bc, 0);
 
     return bc;
 }
--- src/include/compiler/bytecode.h
+++ src/include/compiler/bytecode.h
@@ -34,7 +34,7 @@
 
     #define PACKED  __attribute__((packed))
 
-    #define MAGIC_HEADER 0x43424653     // big-endian SFBC (saffire bytecode)
+    #define MAGIC_HEADER            0x43424653     // big-endian SFBC (saffire bytecode)
 
     #define BYTECODE_CONST_STRING        0
     #define BYTECODE_CONST_NUMERICAL     1
@@ -43,9 +43,12 @@
 
 
     typedef struct _bytecode_binary_header {
-        uint32_t   magic;               // magic number 0x53464243 (SFBC)
-        uint32_t   timestamp;           // Modified timestamp for source file
-        uint32_t   crc;                 // Simple CRC check on the file
+        uint32_t   magic;                       // Magic number 0x53464243 (SFBC)
+        uint32_t   timestamp;                   // Modified timestamp for source file
+        uint32_t   bytecode_len;                // Length of the bytecode
+        uint32_t   bytecode_offset;             // Offset of the bytecode
+        uint32_t   signature_len;               // Length of the GPG check (NULL of none)
+        uint32_t   signature_offset;            // Offset of the GPG signature (NULL if none)
     } PACKED t_bytecode_binary_header;
 
 
@@ -55,32 +58,32 @@
         int  len;           // Length of data
 
         union {
-            char *s;
-            long l;
-            struct _bytecode *code;
-            void *ptr;
-            t_object *obj;
+            char *s;                    // String
+            long l;                     // Long
+            struct _bytecode *code;     // Code block
+            void *ptr;                  // Generic pointer
+            t_object *obj;              // Object
         } data;
     } t_bytecode_constant;
 
 
     typedef struct _bytecode_variable_header {
         int  len;           // Length of data
-        char *s;
+        char *s;            // Name of the variable
     } t_bytecode_variable;
 
 
     typedef struct _bytecode {
         int stack_size;         // Maximum stack size for this bytecode
 
-        int code_len;
-        char *code;
+        int code_len;           // Length of the code
+        char *code;             // Actual binary code
 
-        int constants_len;
-        t_bytecode_constant **constants;
+        int constants_len;                  // Number of constants
+        t_bytecode_constant **constants;    // Pointer to constant array
 
-        int variables_len;
-        t_bytecode_variable **variables;
+        int variables_len;                  // Number of variables
+        t_bytecode_variable **variables;    // Pointer to variables array
     } t_bytecode;
 
 
