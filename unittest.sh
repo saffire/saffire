019e63ab3602feb64605657c2e9924c975ea406f
Joshua Thijssen <jthijssen@noxlogic.nl>
2012-12-24 21:24:17 +0100

    Checking if Saffire is configured properly

--- unittest.sh
+++ unittest.sh
@@ -7,4 +7,10 @@ if [ ! -e $SAFFIRE_TEST_BIN ] ; then
 	exit 1;
 fi
 
+DEBUG=`$SAFFIRE_TEST_BIN --version --long | grep "debug"`
+if [ -n "$DEBUG" ] ; then
+	echo "Please build saffire without debug settings before running this test script";
+	exit 1;
+fi
+
 php unittests/unittester/run-saffire-tests.php ${1:-"unittests/tests/"}
