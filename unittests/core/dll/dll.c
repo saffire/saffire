24adec36c6a1e51cca091fd0a268ae57114db426
Kevin Daudt <compufreak@gmail.com>
2012-12-26 18:13:24 +0100

    unittests: Add test for dll_prepend

--- unittests/core/dll/dll.c
+++ unittests/core/dll/dll.c
@@ -27,11 +27,39 @@ void test_dll_dll_prepend_prepends_item() {
     CU_ASSERT_STRING_EQUAL(DLL_HEAD(dll)->data, "prepend");
 }
 
+void test_dll_dll_insert_before_inserts_item_before() {
+    t_dll *dll = dll_init();
+
+    dll_append(dll, "test1");
+    dll_append(dll, "test3");
+
+    t_dll_element *element = DLL_TAIL(dll);
+
+    dll_insert_before(dll, element, "test2");
+
+    CU_ASSERT_STRING_EQUAL(DLL_NEXT(DLL_HEAD(dll))->data, "test2");
+}
+
+void test_dll_dll_insert_after_inserts_item_after() {
+    t_dll *dll = dll_init();
+
+    dll_append(dll, "test1");
+    dll_append(dll, "test3");
+
+    t_dll_element *element = DLL_HEAD(dll);
+
+    dll_insert_after(dll, element, "test2");
+
+    CU_ASSERT_STRING_EQUAL(DLL_NEXT(DLL_HEAD(dll))->data, "test2");
+}
+
 void test_dll_init() {
      CU_pSuite suite = CU_add_suite("dll", NULL, NULL);
 
      CU_add_test(suite, "dll_init creates object", test_dll_dll_init);
      CU_add_test(suite, "dll_append adds item", test_dll_dll_append_adds_item);
      CU_add_test(suite, "dll_prepend prepends item", test_dll_dll_prepend_prepends_item);
+     CU_add_test(suite, "dll_insert_before inserts item before", test_dll_dll_insert_before_inserts_item_before);
+     CU_add_test(suite, "dll_insert_after inserts item after", test_dll_dll_insert_after_inserts_item_after);
 
 }
