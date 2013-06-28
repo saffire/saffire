fa8a2436cd8d145a17cdb909991f91b2f550aff7
Joshua Thijssen <jthijssen@noxlogic.nl>
2013-06-28 15:14:22 +0200

    Refactored the vm_import

--- src/components/vm/import.c
+++ src/components/vm/import.c
@@ -40,7 +40,7 @@
 /*
  * @TODO: Modules are search in this order on these paths. They are currently hardcoded
  */
-const char *module_search_path[] = {
+const char *module_search_paths[] = {
     ".",
     "/usr/share/saffire/modules",
     "/usr/share/saffire/modules/sfl",
@@ -48,8 +48,16 @@ const char *module_search_path[] = {
 };
 
 
-static t_object *_import_class_from_file(t_vm_frame *frame, char *source_file, char *class, t_vm_frame **module_frame) {
-    DEBUG_PRINT(" * _import_class_from_file(%s, %s)\n", source_file, class);
+/**
+ * Executes a source-file inside a frame, and extracts the needed class from the frame
+ *
+ * @param frame
+ * @param source_file
+ * @param class
+ * @param module_frame
+ * @return
+ */
+static t_vm_frame *_execute_frame(t_vm_frame *frame, char *source_file) {
 
     // @TODO: Don't care about cached bytecode for now! Compile source to BC
     t_ast_element *ast = ast_generate_from_file(source_file);
@@ -58,112 +66,144 @@ static t_object *_import_class_from_file(t_vm_frame *frame, char *source_file, c
     bc->source_filename = smm_strdup(source_file);
 
     // Create a new frame and run it!
-    *module_frame = vm_frame_new(frame, "{import}", bc);
-    vm_execute(*module_frame);
+    t_vm_frame *module_frame = vm_frame_new(frame, "{import}", bc);
+    vm_execute(module_frame);
 
     DEBUG_PRINT("\n\n\n\n * End of running module bytecode.\n");
 
-    t_object *obj = vm_frame_find_identifier(*module_frame, class);
-    DEBUG_PRINT("FOUND: %08X\n", (unsigned int)obj);
-    return obj;
+    return module_frame;
 }
+
+/**
+ * Build actual class-path from a module (Framework::View -> Framework/View)
+ *
+ * @param path
+ * @return
+ */
+static char *build_class_path(char *module) {
+    char *class_path = smm_strdup(module);
+    char *c;
+
+    // We find all '::', replace the first character with '/', and move all
+    // remaining chars in the string up one position
+    while (c = strstr(class_path, "::"), c != NULL) {
+        *c = '/';
+        memmove(c+1, c+2, strlen(c)+1);
+    }
+
+    return class_path;
+}
+
+/**
+ * Construct a absolute path from a root and sub path.
+ *
+ * @param root_path
+ * @param sub_path
+ * @return
+ */
+static char *construct_import_path(char *root_path, char *sub_path) {
+    char *path = realpath(root_path, NULL);
+    if (! path) return NULL;
+
+    char *final_path;
+    asprintf(&final_path, "%s/%s.sf", root_path, sub_path);
+    DEBUG_PRINT(" * *** Constructed path: '%s'\n", final_path);
+    return final_path;
+}
+
+
+/**
+ * Search all import paths for class_path, and extracts+caches the needed class from that file
+ *
+ * @param frame
+ * @param class_path
+ * @param class
+ * @param file_found
+ * @return
+ */
+static t_object *search_import_paths(t_vm_frame *frame, char *class_path, char *class, int *file_found) {
+    *file_found = 0;
+
+    char **search_root_path = (char **)&module_search_paths;
+    while (*search_root_path) {
+        char *final_search_path = construct_import_path(*search_root_path, class_path);
+
+        *file_found = is_file(final_search_path) ? 1 : 0;
+
+        if (*file_found) {
+            t_vm_frame *import_frame = _execute_frame(frame, final_search_path);
+            t_object *obj = vm_frame_find_identifier(import_frame, class);
+
+            // Add frame to cache
+            DEBUG_PRINT(" * *** Adding to import cache at key '%s'\n", class_path);
+            ht_add(import_cache, class_path, import_frame);
+
+            // free instead of smm_free, since construct_import_path() allocates through asprintf()
+            free(final_search_path);
+
+            return obj;
+        }
+
+        // free instead of smm_free, since construct_import_path() allocates through asprintf()
+        free(final_search_path);
+
+        // Check next root path
+        search_root_path++;
+    }
+
+    return NULL;
+}
+
 
 /**
  *
  */
 t_object *vm_import(t_vm_frame *frame, char *module, char *class) {
-    char fqcn[256];
+    t_object *obj = NULL;
+    int file_found = 0;
 
-    DEBUG_PRINT("\n\n\n");
-    DEBUG_PRINT("*** looks like we need to import class '%s' from module '%s'\n", class, module);
-
-    snprintf(fqcn, 255, "%s::%s", module, class);
+    DEBUG_PRINT("Importing class '%s' from module '%s'\n", class, module);
+    char *fqcn = smm_malloc(strlen(module) + strlen(class) + strlen("::") + 1);
+    sprintf(fqcn, "%s::%s", module, class);
     DEBUG_PRINT(" * Looks like we're looking for '%s'\n", fqcn);
-    t_object *obj = vm_frame_find_identifier(frame, fqcn);
-
+    obj = vm_frame_find_identifier(frame, fqcn);
+    smm_free(fqcn);
     // Found our object, return
     if (obj != NULL) {
+        DEBUG_PRINT(" * *** Found as object inside our frame\n");
         return obj;
     }
 
-    DEBUG_PRINT(" * *** Looking for frame in cache: \n");
-    t_vm_frame *cached_frame = ht_find(import_cache, fqcn);
-    if (cached_frame) {
-        t_object *obj = vm_frame_find_identifier(cached_frame, class);
-        DEBUG_PRINT(" * *** *** CACHED FOUND: %08X\n", (unsigned int)obj);
-        return obj;
-    }
+
 
     DEBUG_PRINT(" * *** Importing file. Scanning searchpath:\n");
+    char *class_path = build_class_path(module);
 
-    // Use the module to build part of the path. "Module" => "Module", "Module::Submodule" => "Module/Submodule".
-    char *module_path = smm_malloc(strlen(module) + 1);
-    char *separator_pos = strstr(module, "::");
-    if (separator_pos == NULL) {
-        // There's no namespace separator, so just use the whole string.
-        strcpy(module_path, module);
+    // CHeck if we already executed the frame before. If so, it's stored in cache
+    t_vm_frame *cached_frame = ht_find(import_cache, class_path);
+    DEBUG_PRINT(" * *** Looking for a frame in cache that holds '%s': %s\n", class_path, cached_frame ? "Found" : "Nothing found");
+    if (cached_frame) {
+        // Fetch the object from the frame
+        obj = vm_frame_find_identifier(cached_frame, class);
+        file_found = 1;
     } else {
-        // We need extra vars to track where we're copying to.
-        char *module_path_loc = module_path;
-        char *module_loc = module;
-
-        // Loop over each separator, adding it to our path string.
-        do {
-            int len = separator_pos - module_loc;
-            DEBUG_PRINT("Copying %d bytes from %d to %d\n", len, module_loc, module_path_loc);
-            strncpy(module_path_loc, module_loc, len);
-
-            DEBUG_PRINT("Incrementing module_loc to %d\n", separator_pos + 2);
-            module_loc = separator_pos + 2;
-
-            DEBUG_PRINT("Incrementing %s to %d\n", module_path_loc, module_path_loc + len);
-            module_path_loc += len;
-            *module_path_loc = '/';
-            module_path_loc++;
-        } while ((separator_pos = strstr(module_loc, "::")) != NULL);
-
-        // Copy the last part of the module.
-        strncpy(module_path_loc, module_loc, strlen(module_loc));
-
-        DEBUG_PRINT("   * Pointers are at %d and %d\n", module_path, module_path_loc);
-    }
-    DEBUG_PRINT("   * Built module path: '%s'\n", module_path);
-
-    // Scan . and /usr/saffire/modules only!
-    char **current_search_path = (char **)&module_search_path;
-    while (*current_search_path) {
-        DEBUG_PRINT("   * Searching on path '%s'\n", *current_search_path);
-        char final_path[PATH_MAX];
-        char *path = realpath(*current_search_path, NULL);
-        if (path) {
-            snprintf(final_path, PATH_MAX, "%s/%s.sf", path, module_path);
-            DEBUG_PRINT("   * Looking for module at path '%s'\n", final_path);
-
-            if (is_file(final_path)) {
-                DEBUG_PRINT("   * Found a matching file. Whoohoo!\n");
-                t_vm_frame *import_frame;
-                t_object *obj = _import_class_from_file(frame, final_path, class, &import_frame);
-                if (! obj) {
-                    object_raise_exception(Object_ImportException, "Cannot find class '%s' in module '%s'", class, module);
-                    smm_free(module_path);
-                    return NULL;
-                }
-
-                // Add frame to cache
-                DEBUG_PRINT("   * Adding to import cache at key '%s'\n", fqcn);
-                ht_add(import_cache, fqcn, import_frame);
-                smm_free(module_path);
-                return obj;
-            } else {
-                DEBUG_PRINT("   * Nothing found here.. continuing..\n");
-            }
-        } else {
-            DEBUG_PRINT("   * This path is not real!?");
-        }
-        current_search_path++;
+        obj = search_import_paths(frame, class_path, class, &file_found);
     }
 
-    object_raise_exception(Object_SystemException, "Cannot find module '%s'", module);
-    smm_free(module_path);
-    return NULL;
+    smm_free(class_path);
+
+    // No matching file found
+    if (! file_found) {
+        object_raise_exception(Object_SystemException, "Cannot find module '%s'", module);
+        return NULL;
+    }
+
+    // No object found in file
+    if (! obj) {
+        object_raise_exception(Object_ImportException, "Cannot find class '%s' in module '%s'", class, module);
+        return NULL;
+    }
+
+    // All ok
+    return obj;
 }
