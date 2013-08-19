f550917b0a4c1e78549585ef67fd4394f6b78eb7
Joshua Thijssen <jthijssen@noxlogic.nl>
2013-08-19 15:39:18 +0200

    Small (but anoying) fix in bytecode dumper

--- support/readdump/bytecode/Constant/Numerical.php
+++ support/readdump/bytecode/Constant/Numerical.php
@@ -6,4 +6,9 @@ use bytecode\Constant\AbstractConstant;
 
 class Numerical extends AbstractConstant {
 
+    function __construct($data) {
+        $data = unpack("Vval", $data);
+        $this->data = $data['val'];
+    }
+
 }
