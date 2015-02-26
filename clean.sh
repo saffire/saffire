2ca8dc95792ec3abd792c6a0f2266e60a8560eca
Joshua Thijssen <jthijssen@noxlogic.nl>
2015-02-26 12:30:32 +0100

    Small fixes in scripts

--- clean.sh
+++ clean.sh
@@ -5,8 +5,8 @@ rm -rf build/release
 rm src/include/compiler/lex.yy.h
 rm src/components/compiler/lex.yy.c
 rm src/components/compiler/parser.tab.c
-rm src/components/vm/_generated_opcodes.c
-rm src/include/vm/_generated_opcodes.h
+rm src/components/vm/_generated_vm_opcodes.c
+rm src/include/vm/_generated_vm_opcodes.h
 rm src/components/objects/_generated_exceptions.inc
 rm src/include/objects/_generated_exceptions.h
 rm src/components/objects/_generated_interfaces.inc
