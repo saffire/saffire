995bb2ce58da62d2caa9b6302f59cf9b61fe3790
Joshua Thijssen <jthijssen@noxlogic.nl>
2015-02-26 12:00:02 +0100

    Fixed more travis and file generation

--- clean.sh
+++ clean.sh
@@ -5,3 +5,9 @@ rm -rf build/release
 rm src/include/compiler/lex.yy.h
 rm src/components/compiler/lex.yy.c
 rm src/components/compiler/parser.tab.c
+rm src/components/vm/_generated_opcodes.c
+rm src/include/vm/_generated_opcodes.h
+rm src/components/objects/_generated_exceptions.inc
+rm src/include/objects/_generated_exceptions.h
+rm src/components/objects/_generated_interfaces.inc
+rm src/include/objects/_generated_interfaces.h
