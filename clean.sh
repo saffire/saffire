45b4922bcb37ada0208f3119534897e533602676
Joshua Thijssen <jthijssen@noxlogic.nl>
2015-03-02 10:27:10 +0100

    Forcing cleanup of files without errors

--- clean.sh
+++ clean.sh
@@ -2,17 +2,18 @@
 # Cleans up the builds
 rm -rf build/debug
 rm -rf build/release
-rm src/include/compiler/lex.yy.h
-rm src/components/compiler/lex.yy.c
-rm src/components/compiler/parser.tab.c
-rm src/components/vm/_generated_vm_opcodes.c
-rm src/include/vm/_generated_vm_opcodes.h
-rm src/components/objects/_generated_exceptions.inc
-rm src/include/objects/_generated_exceptions.h
-rm src/components/objects/_generated_interfaces.inc
-rm src/include/objects/_generated_interfaces.h
-rm src/include/config.h
-rm src/include/gitversion.h
-rm src/include/compiler/parser.tab.h
 
-true
+rm -f src/components/compiler/lex.yy.c
+rm -f src/components/compiler/parser.tab.c
+rm -f src/components/vm/_generated_vm_opcodes.c
+rm -f src/components/objects/_generated_exceptions.inc
+rm -f src/components/objects/_generated_interfaces.inc
+
+rm -f include/compiler/lex.yy.h
+rm -f include/vm/_generated_vm_opcodes.h
+rm -f include/objects/_generated_exceptions.h
+rm -f include/objects/_generated_interfaces.h
+rm -f include/config.h
+rm -f include/gitversion.h
+rm -f include/compiler/parser.tab.h
+
