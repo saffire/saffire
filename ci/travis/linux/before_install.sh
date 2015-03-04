cc8b2e634bfafeda5c2c60edbe07f7ca6a65bc26
Joshua Thijssen <jthijssen@noxlogic.nl>
2015-03-04 13:17:50 +0100

    Merged plus some smaller updates

---
+++ ci/travis/linux/before_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update -qq
+sudo apt-get install -qq libpcre3-dev libfcgi-dev libedit-dev libbz2-dev libxml2-dev
+sudo apt-get install -qq php5-cli
+sudo apt-get install -qq libcunit1-dev
