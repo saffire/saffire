ee1c9309664cd0e86ebe60a9fb0d3fd6c8b07f9b
Stefano Azzalini <stefano.azzalini@yahoo.com>
2015-03-06 11:14:10 +0100

    Polish

--- clean.sh
+++ clean.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-# Cleans up the builds
-rm -rf build
 
-rm -f src/components/compiler/lex.yy.c
-rm -f src/components/compiler/parser.tab.c
-rm -f src/components/vm/_generated_vm_opcodes.c
-rm -f src/components/objects/_generated_exceptions.inc
-rm -f src/components/objects/_generated_interfaces.inc
+# Cleans up the builds
+
+rm -rf build
 
 rm -f include/saffire/compiler/lex.yy.h
 rm -f include/saffire/vm/_generated_vm_opcodes.h
@@ -15,3 +11,9 @@ rm -f include/saffire/objects/_generated_interfaces.h
 rm -f include/saffire/config.h
 rm -f include/saffire/gitversion.h
 rm -f include/saffire/compiler/parser.tab.h
+
+rm -f src/components/compiler/lex.yy.c
+rm -f src/components/compiler/parser.tab.c
+rm -f src/components/vm/_generated_vm_opcodes.c
+rm -f src/components/objects/_generated_exceptions.inc
+rm -f src/components/objects/_generated_interfaces.inc
