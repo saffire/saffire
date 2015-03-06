6a38223d089e189818bd30243ca8a8200d3fbc9d
Stefano Azzalini <stefano.azzalini@yahoo.com>
2015-03-06 11:11:30 +0100

    Fix path to files to remove

--- clean.sh
+++ clean.sh
@@ -9,11 +9,10 @@ rm -f src/components/vm/_generated_vm_opcodes.c
 rm -f src/components/objects/_generated_exceptions.inc
 rm -f src/components/objects/_generated_interfaces.inc
 
-rm -f include/compiler/lex.yy.h
-rm -f include/vm/_generated_vm_opcodes.h
-rm -f include/objects/_generated_exceptions.h
-rm -f include/objects/_generated_interfaces.h
-rm -f include/config.h
-rm -f include/gitversion.h
-rm -f include/compiler/parser.tab.h
-
+rm -f include/saffire/compiler/lex.yy.h
+rm -f include/saffire/vm/_generated_vm_opcodes.h
+rm -f include/saffire/objects/_generated_exceptions.h
+rm -f include/saffire/objects/_generated_interfaces.h
+rm -f include/saffire/config.h
+rm -f include/saffire/gitversion.h
+rm -f include/saffire/compiler/parser.tab.h
