5b1dcfee3b9aacdf717420da385e4036d768f943
Joshua Thijssen <jthijssen@noxlogic.nl>
2015-08-09 23:19:50 +0200

    Implmeneted code coverage gcov target in cmake

--- unittest.sh
+++ unittest.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-export SAFFIRE_TEST_BIN=./build/release/bin/saffire
+CPWD=`pwd`
+
+cd `dirname $0`
+
+: ${SAFFIRE_TEST_BIN:=./build/release/bin/saffire}
+export SAFFIRE_TEST_BIN
 
 if [ ! -e $SAFFIRE_TEST_BIN ] ; then
-	echo "Please build saffire before running this test script";
-	exit 1;
+    echo "Cannot find the test binary '$SAFFIRE_TEST_BIN'";
+    exit 1;
 fi
 
 DEBUG=`$SAFFIRE_TEST_BIN version --long | grep "debug"`
 if [ -n "$DEBUG" ] ; then
-	echo "Please build saffire without debug settings before running this test script";
-	exit 1;
+    echo "Please build saffire without debug settings before running this test script";
+    exit 1;
 fi
 
-./build/release/bin/utmain
+`dirname ${SAFFIRE_TEST_BIN}`/utmain
 /usr/bin/php support/unittester/run-saffire-tests.php ${1:-"unittests/tests/"}
+
+cd $CPWD
